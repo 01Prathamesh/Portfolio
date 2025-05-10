c500cb9f6c7f554e67590286e0fae96ae496e461
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-05-10 11:04:20 +0530

    chore(resume): Update resume download link to latest Google Drive version
    - Replaced outdated resume URL with new link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = ({ darkMode }) => {
             <i className="bi bi-linkedin fs-4"></i>
           </a>
           <a
-            href="https://drive.google.com/file/d/1frXb24ETZwmhmivNKtw8RIA8gzQIzYAz/view?usp=sharing"
+            href="https://drive.google.com/file/d/1UksqrqTD363_ddaa5qZ-uJbsOO46-UaN/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
             className="mx-3 text-reset"
